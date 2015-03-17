6dcbefc7fe1232e92c224bfd044150f9564032d2
Remi Nivet <remi.nivet@worldline.com>
2015-03-17 09:53:06 +0100

    Add copyright headers back
    Change-Id: I2fd71a881efe3fa0b66fa74afaf689c478610ac5

--- meta2v2/meta2_backend_internals.h
+++ meta2v2/meta2_backend_internals.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2015 Worldline
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef META2V2_INTERNALS__H
 # define META2V2_INTERNALS__H 1
 # include <stdlib.h>
