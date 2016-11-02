79e6e38a8880d41b7aa66f1195a7a8a136832cf5
Remi Nivet <remi.nivet@worldline.com>
2016-11-02 18:13:27 +0100

    Add automatic election expiration after 300s to limit election storm
    Change-Id: I1494f3c2f41a758effc0e37f2617e469511243dc

--- sqliterepo/replication_dispatcher.c
+++ sqliterepo/replication_dispatcher.c
@@ -1359,21 +1359,22 @@ sqlx_dispatch_GETVERS(struct gridd_reply_ctx_s *reply,
 	EXTRACT_STRING("BASE_TYPE", type);
 	reply->subject("%s.%s", base, type);
 
-	// TODO JFS : trigger an election, useful to reduce the number of
-	// messages during an election.
-	err = sqlx_repository_use_base(repo, type, base);
-	if (NULL != err) {
-		g_prefix_error(&err, "Use: ");
-		reply->send_error(0, err);
-		return TRUE;
-	}
-
 	err = sqlx_repository_open_and_lock(repo, type, base,
 			SQLX_OPEN_LOCAL, &sq3, NULL);
 
 	if (NULL != err) {
 		g_prefix_error(&err, "Open/lock: ");
 		reply->send_error(0, err);
+		return TRUE;
+	}
+
+	// TODO JFS : trigger an election, useful to reduce the number of
+	// messages during an election.
+	err = sqlx_repository_use_base(repo, type, base);
+	if (NULL != err) {
+		g_prefix_error(&err, "Use: ");
+		reply->send_error(0, err);
+		sqlx_repository_unlock_and_close_noerror(sq3);
 		return TRUE;
 	}
 
@@ -1422,19 +1423,20 @@ sqlx_dispatch_REPLICATE(struct gridd_reply_ctx_s *reply,
 
 	reply->send_reply(100, "received");
 
+	err = sqlx_repository_open_and_lock(repo, type, base,
+			SQLX_OPEN_LOCAL|SQLX_OPEN_CREATE, &sq3, NULL);
+	if (NULL != err) {
+		reply->send_error(0, err);
+		return TRUE;
+	}
+
 	/* Starts an election without being an initiator ... because I receive
 	 * this request from a master, so an election is already running
 	 * somewhere else. */
 	err = sqlx_repository_use_base(repo, type, base);
 	if (NULL != err) {
 		reply->send_error(0, err);
-		return TRUE;
-	}
-
-	err = sqlx_repository_open_and_lock(repo, type, base,
-			SQLX_OPEN_LOCAL|SQLX_OPEN_CREATE, &sq3, NULL);
-	if (NULL != err) {
-		reply->send_error(0, err);
+		sqlx_repository_unlock_and_close_noerror(sq3);
 		return TRUE;
 	}
 
@@ -1741,24 +1743,29 @@ sqlx_dispatch_RESYNC(struct gridd_reply_ctx_s *reply,
 	EXTRACT_STRING("BASE_TYPE", type);
 	reply->subject("%s.%s", base, type);
 
+	struct sqlx_sqlite3_s *sq3 = NULL;
+	err = sqlx_repository_open_and_lock(repo, type, base,
+			SQLX_OPEN_SLAVEONLY, &sq3, NULL);
+	if (NULL != err) {
+		reply->send_error(0, err);
+		return TRUE;
+	}
+
 	/* Force refresh of peers from meta1 */
 	em = sqlx_repository_get_elections_manager(repo);
 	err = sqlx_config_has_peers2(election_manager_get_config(em),
 			base, type, TRUE, &has_peers);
+	if (NULL != err) {
+		reply->send_error(0, err);
+		return TRUE;
+	}
 	g_clear_error(&err);
 
 	/* Open and lock the base */
 	err = sqlx_repository_use_base(repo, type, base);
 	if (NULL != err) {
 		reply->send_error(0, err);
-		return TRUE;
-	}
-
-	struct sqlx_sqlite3_s *sq3 = NULL;
-	err = sqlx_repository_open_and_lock(repo, type, base,
-			SQLX_OPEN_SLAVEONLY, &sq3, NULL);
-	if (NULL != err) {
-		reply->send_error(0, err);
+		sqlx_repository_unlock_and_close_noerror(sq3);
 		return TRUE;
 	}
 
